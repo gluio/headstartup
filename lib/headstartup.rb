bc0b934174d4a3d68657d9d468f1e7b262911bfa
Glenn Gillen <me@glenngillen.com>
2015-03-23 09:58:40 +1100

    Move landing pages into their own directory.

--- lib/headstartup.rb
+++ lib/headstartup.rb
@@ -1,7 +1,14 @@
 require "headstartup/version"
+require "nesta/config"
 require "nesta/app"
 
 module Headstartup
+  class Config < Nesta::Config
+    def self.page_path(basename = nil)
+      get_path(File.join(content_path, "landing-pages"), basename)
+    end
+  end
+
   class App < Nesta::App
   end
 end
