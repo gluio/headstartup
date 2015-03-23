8aaeea8553b9d7394de4fa343b938017e132852c
Glenn Gillen <me@glenngillen.com>
2015-03-23 11:33:00 +1100

    Make it easier to override templates within the hosting app.

--- lib/headstartup.rb
+++ lib/headstartup.rb
@@ -14,30 +14,29 @@ module Nesta
       Nesta::Config.landing_page_path(basename)
     end
   end
+
+  module Overrides
+    alias_method :render_options_without_headstart, :render_options
+
+    def self.render_options(template, *engines)
+      theme_view_path = Nesta::Path.themes('headstartup', 'views')
+      local_view_path = Nesta::Path.local("views/landing-pages")
+      [local_view_path, theme_view_path].each do |path|
+        engines.each do |engine|
+          if template_exists?(engine, path, template)
+            return { views: path }, engine
+          end
+        end
+      end
+      render_options_without_headstart
+    end
+  end
 end
 
 module Headstartup
   class App < Nesta::App
-    get '/sitemap.xml' do
-      content_type :xml, charset: 'utf-8'
-      @pages = LandingPage.find_all.reject do |page|
-        page.draft? or page.flagged_as?('skip-sitemap')
-      end
-      @last = @pages.map { |page| page.last_modified }.inject do |latest, page|
-        (page > latest) ? page : latest
-      end
-      haml(:sitemap, format: :xhtml, layout: false)
-    end
-
-    get '*' do
-      set_common_variables
-      parts = params[:splat].map { |p| p.sub(/\/$/, '') }
-      @page = LandingPage.find_by_path(File.join(parts))
-      raise Sinatra::NotFound if @page.nil?
-      @title = @page.title
-      set_from_page(:description, :keywords)
-      haml(@page.template, layout: @page.layout)
-    end
+    app_file = Nesta::Path.themes('headstartup', 'app.rb')
+    require app_file if File.exist?(app_file)
   end
 end
 
